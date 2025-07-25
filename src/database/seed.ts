95a60140cd1b592657142b110599c02bb894e29a
cal-oliveira <calebe81899026@gmail.com>
2025-07-24 23:10:47 -0300

    feat: create seed

---
+++ src/database/seed.ts
@@ -0,0 +1,59 @@
+import { type SQLiteDatabase } from "expo-sqlite";
+
+const productNames: string[] = [
+  "Arroz",
+  "Feijão",
+  "Macarrão",
+  "Açúcar",
+  "Sal",
+  "Óleo de soja",
+  "Café",
+  "Leite",
+  "Farinha de trigo",
+  "Farinha de mandioca",
+  "Pão de forma",
+  "Biscoito",
+  "Manteiga",
+  "Margarina",
+  "Queijo",
+  "Presunto",
+  "Ovos",
+  "Batata",
+  "Cebola",
+  "Alho",
+  "Tomate",
+  "Cenoura",
+  "Beterraba",
+  "Abobrinha",
+  "Chuchu",
+  "Alface",
+  "Couve",
+  "Espinafre",
+  "Repolho",
+  "Maçã",
+  "Banana",
+  "Laranja",
+  "Mamão",
+  "Melancia",
+  "Uva",
+  "Pera",
+  "Abacaxi",
+  "Limão",
+  "Morango",
+  "Iogurte",
+  "Requeijão",
+  "Creme de leite",
+  "Leite condensado",
+  "Molho de tomate",
+  "Extrato de tomate",
+  "Milho verde",
+  "Ervilha",
+];
+
+export async function seed(database: SQLiteDatabase) {
+  await database.withTransactionAsync(async () => {
+    for (const name of productNames) {
+      await database.runAsync("INSERT INTO products (name) VALUES (?)", [name]);
+    }
+  });
+}
